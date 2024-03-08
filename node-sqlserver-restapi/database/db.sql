32dda016d47cd0cc5aa8f5eade1410390121a142
Facundo Veron <faccu.veron92@gmail.com>
2024-03-08 13:25:47 -0300

    Muchos cambios

---
+++ node-sqlserver-restapi/database/db.sql
@@ -0,0 +1,13 @@
+-- create database webstore
+
+-- CREATE TABLE products (
+-- id INT IDENTITY(1,1) PRIMARY KEY,
+-- name VARCHAR(100) NOT NULL,
+-- price DECIMAL(10,2) NOT NULL,
+-- quantity INT,
+-- description TEXT
+-- );
+
+use webstore;
+
+INSERT INTO products (name, price, quantity, description) VALUES ('Mouse','Mouse gaming', 10, 100);
---
+++ node-sqlserver-restapi/src/app.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import productRoutes from './routes/products.routes.js'
+
+const app = express();
+app.use(express.json());
+
+app.use(productRoutes);
+
+export default app
